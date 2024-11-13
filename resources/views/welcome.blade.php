1693b579933ea8520c3cb4fc5825bf6f47e09ddf
backend <backend@sp.ae>
2024-11-13 13:47:24 +0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 text-center p-10">
+
+    <h1 class="text-2xl text-slate-900">Elal Rayan Chaneel</h1>
+    <h2 class="text-xl text-red-900">Laravel</h2>
+    <span class="text-amber-400">this course for biggeneres</span>
+    <h2 class="text-xl  text-red-900">TailwindCss</h2>
+    <span class="text-amber-400">this course for frontend </span>
+
+</body>
+</html>
